05220157d63c5d09ba1c9fe5b9a03f67cce490a0
minjyo <minjyo8823@gmail.com>
2019-11-19 02:38:34 +0900

    feat: add main page + project structure 1. main page design 2. routing in App.js 3. set project structure -> refer to "requirement.txt"

--- ratel/frontend/src/App.js
+++ ratel/frontend/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "component/Header";
+import MainCT from "screens/Main/MainCT";
+import MenuCT from "screens/Menu/MenuCT";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Header></Header>
+                <Switch>
+                    <Route exact path="/" component={MainCT} />
+                    <Route path="/menu1" component={MenuCT} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ ratel/frontend/src/component/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.scss";
+
+const Footer = props => {
+    return (
+        <div className="footer">
+            <div className="content">@copyrighted by ratel</div>
+        </div>
+    );
+};
+
+export default Footer;
