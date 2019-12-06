17acaed74bbe6949ccafccfbef540c9f0a0d6c9d
Koowin <koowin42@gmail.com>
2019-12-06 21:44:33 +0900

    add model, bdfunc, miration

--- ratel/ratelweb/models.py
+++ ratel/ratelweb/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email = models.CharField(max_length=50, default="aaa@gmail.com")
+
+class Bookmark(models.Model):
+    iden = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    bookmark = models.IntegerField(default=1234567891011)
